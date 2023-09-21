927ca5bf61df7d08c7957d27e817ec0070117b31
Mrinall Umasudhan <mrinalluma@gmail.com>
2023-09-21 02:08:34 -0400

    css fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/Getting Started/Introduction">
+            to="docs/category/getting-started">
             Proceed to Guide!
           </Link>
         </div>

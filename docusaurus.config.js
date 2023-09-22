f6251e08632169e89521d9e3b699a4f9bca40dce
Restyled.io <commits@restyled.io>
2023-09-22 07:01:24 +0000

    Restyled by prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,108 +1,105 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title : 'Tech Toolbox',
-  tagline : 'Authors: Mrinall Umasudhan and Parth Goyal',
-  favicon : 'img/favicon.png',
+  title: "Tech Toolbox",
+  tagline: "Authors: Mrinall Umasudhan and Parth Goyal",
+  favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url : 'https://ftc-tech-toolbox.vercel.app/',
+  url: "https://ftc-tech-toolbox.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl : '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName : 'Tech Toolbox', // Usually your GitHub org/user name.
-  projectName : 'Tech Toolbox',      // Usually your repo name.
+  organizationName: "Tech Toolbox", // Usually your GitHub org/user name.
+  projectName: "Tech Toolbox", // Usually your repo name.
 
-  onBrokenLinks : 'throw',
-  onBrokenMarkdownLinks : 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n : {
-    defaultLocale : 'en',
-    locales : [ 'en' ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
-  presets : [
+  presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs : {
-          sidebarPath : require.resolve('./sidebars.js'),
-          remarkPlugins : [ math ],
-          rehypePlugins : [ katex ],
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
-        blog : {
-          showReadingTime : true,
-
+        blog: {
+          showReadingTime: true,
         },
-        theme : {
-          customCss : require.resolve('./src/css/custom.css'),
-
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
-  stylesheets : [
+  stylesheets: [
     {
-      href : 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type : 'text/css',
-      integrity :
-          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin : 'anonymous',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
-  themeConfig :
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        // Replace with your project's social card
-        docs : {
-          sidebar : {
-            autoCollapseCategories : true,
-            hideable : true,
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+          hideable: true,
+        },
+      },
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Tech Toolbox",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/favicon.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Software",
           },
-        },
-        image : 'img/docusaurus-social-card.jpg',
-        navbar : {
-          title : 'Tech Toolbox',
-          logo : {
-            alt : 'My Site Logo',
-            src : 'img/favicon.png',
+          {
+            href: "https://github.com/MrinallU/FTC-Tech-Toolbox",
+            label: "GitHub",
+            position: "right",
           },
-          items : [
-            {
-              type : 'docSidebar',
-              sidebarId : 'tutorialSidebar',
-              position : 'left',
-              label : 'Software',
-            },
-            {
-              href : 'https://github.com/MrinallU/FTC-Tech-Toolbox',
-              label : 'GitHub',
-              position : 'right',
-            },
-
-          ],
-        },
-        prism : {
-          theme : lightCodeTheme,
-          darkTheme : darkCodeTheme,
-          additionalLanguages : [ 'java' ],
-        },
-      }),
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["java"],
+      },
+    }),
 };
 
 module.exports = config;

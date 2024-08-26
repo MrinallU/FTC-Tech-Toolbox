a581d929e935a7e08818ad069a483851314e445c
Mrinall Umasudhan <mrinalluma@gmail.com>
2024-08-26 14:34:37 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://ftc-tech-toolbox.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/category/getting-started",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

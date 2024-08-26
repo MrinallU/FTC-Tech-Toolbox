e696a7cae73405fafebc41efac91fa553095c6cd
Restyled.io <commits@restyled.io>
2024-08-26 19:12:23 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import { Analytics } from '@vercel/analytics/react';
+import Layout from '@theme/Layout';
+import {Analytics} from '@vercel/analytics/react';
+import clsx from 'clsx';
+import React from 'react';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
- 
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -17,7 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+  className = "button button--secondary button--lg"
             to="docs/category/getting-started">
             Proceed to Guide!
           </Link>
@@ -29,7 +30,5 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <Redirect to="/docs/category/getting-started" />
-  );
+  return (<Redirect to = "/docs/category/getting-started" />);
 }
